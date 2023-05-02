adf8c31eaf750c49d69002771dbb82d69f11cc7f
ducmanh <manhpd151290@fpt.edu.vn>
2023-05-03 00:58:46 +0700

    replace background register

--- pages/register.tsx
+++ pages/register.tsx
@@ -15,7 +15,7 @@ const RegisterPage: NextPageWithLayout = () => {
 RegisterPage.getLayout = function getLayout(page: ReactElement) {
   return (
     <EndUserLayout>
-      <Image src="/login-background.jpg" layout="fill" />
+      <Image src="/asset/slider3.jpeg" layout="fill" />
       {page}
     </EndUserLayout>
   )
